828c74743a313ccd9a1811558e806bb25876275c
3ff3ct <56245506+3ff3ct@users.noreply.github.com>
2020-01-08 23:17:03 -0600

    Delete wpscan.sh

--- wpscan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# install wpscan
-
-sudo apt update
-sudo apt install ruby build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev -y
-sudo gem install wpscan
-wpscan --version
